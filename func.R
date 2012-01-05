a758b37c7725a1cdd8d658be3c09ec7edd46a3e6
richie <richie.morrisroe@gmail.com>
2012-01-05 21:48:16 +0000

    added useful functions

---
+++ func.R
@@ -0,0 +1,71 @@
+FactorXtab <-  function (x) {
+x.load<-x$loadings 
+x.comm<-x$communality
+x.names <- colnames(x.load)
+len <- length(colnames(x.load))
+x.names[len+1] <- "Communalities"
+x.comm.load<-cbind(x.load, x.comm)
+x.mat.df<-as.matrix.data.frame(x.comm.load)
+colnames(x.mat.df) <- x.names
+#colnames(x.mat.df)[length(x.mat.df)] <- "Communalities"
+fact.xtab <- xtable(x.mat.df)
+fact.xtab
+}
+FactorCor <- function (x) {
+  res <- x$score.cor
+  allnames <- attr(x$weights, "dimnames")
+  factnames <- allnames[[2]]
+  res <- as.data.frame(res)
+  #colnames(res) <- factnames
+  res.x <- xtable(res)
+}
+ExtractLoadings <- function (x, loadings=0.3) {
+  x.load <- x$loadings
+  x.uc.mat <- as.data.frame(unclass(x.load))
+  xitemsload <- apply(x.uc.mat, 2, function (y) names(y[which(abs(y)>=loadings)])) #return the names of items which have appreciable loadings
+  xitemsload
+}
+## FactorCor <- function (x) {
+##   cor <- x$r.scores
+##  xtable(cor)
+## }
+Extracth2u2 <- function (x) {
+  x.comm <- x$communality
+  x.uniq <- x$uniquenesses
+  x.ratio <- x.comm/x.uniq
+  x.h2u2 <- as.data.frame(cbind(x.comm, x.uniq, x.ratio))
+  x.h2u2
+ }
+IrtXtab <- function (x) {
+  eta<-x$etapar #$
+  se<-x$se.eta #$
+  eta.mat<-as.matrix(eta)
+  se.eta.mat<-as.matrix(se)
+  eta.par.mat<-cbind(eta.mat, se.eta.mat)
+  colnames(eta.par.mat) <- c("Ability Estimate", "Standard Error")
+  coef.xtab<-xtable(eta.par.mat)
+  coef.xtab
+}
+coefreshape <-  function (x) {
+#  mxlength <- lapply(x, max.length)
+  res <- matrix(NA, ncol=length(x), nrow=10)
+  rownames(x) <- names(x[3])
+  for (i in seq_along(x)) {
+    y <- get(x[i])
+    res[,i] <- y[]
+  }
+  res
+}
+FitIndices <- function (x) {
+  tli <- x$TLI
+  bic <- x$BIC
+  rmsea <- x$RMSEA
+  rmsnames <- attr(x$RMSEA, "names")
+  res <- as.data.frame(cbind(tli, bic, rmsea[1], rmsea[2], rmsea[3]))
+  print(length(res))
+  colnames(res) <- c(paste(substitute(x),"TLI", sep=""),paste(substitute(x),"BIC", sep=""),
+                     paste(substitute(x),"RMSEA", sep=""),
+                     paste(substitute(x),"-90CInt", sep=""),
+                     paste(substitute(x),"+90CInt", sep=""))
+  res
+}
