8ecef9a17e170be98ae7b1c4b3cc9b42d1847ef4
richie <richie.morrisroe@gmail.com>
2013-02-03 11:07:29 +0000

    updated penalisedRegression function to return dataframe with pred and obs columns

--- func.R
+++ func.R
@@ -675,10 +675,24 @@ penalisedRegression <- function(x, y, traindata, testdata, newy, alpha, nfolds=1
   mod <- glmnet(x=x.mat, y=y, alpha=alpha)
   pred.coef <- predict(mod, testdata.mat, s=cvres$lambda.min, type=type)
   if(type=="response") {
-  pred <- data.frame(pred=pred.coef, obs=newy)
+  pred <- data.frame(pred=as.vector(pred.coef), obs=newy)
   return(pred)
 }
   else{
     return(pred.coef)
   }
   }
+tuneLoess <- function(formula, data, newdata, tuneLength, ...) {
+  formula <- as.formula(formula)
+  seq <- seq(0, 1, by=tuneLength)
+  part <- list(length=length(seq))
+  part.index <- createDataPartition(formula[2], p=0.8, times=length(seq))
+  for (i in seq_along(part.index)) {
+    part[[i]] <- data[part.index,]
+  }
+  fitlist <- vector(mode="list", length=length(seq))
+  for (j in seq_along(tuneLength)) {
+    fitlist[[j]] <- loess(formula, data=part[[j]], span=seq[i])
+          }
+    fitlist
+}
