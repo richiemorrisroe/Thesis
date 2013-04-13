562fefcd0eb57a4d04cbe814e84ceac0ddf0c75b
richie <richie.morrisroe@gmail.com>
2013-04-13 15:04:04 +0100

    started performing some data sanity checks on the input files for physiological data and pain scores

--- ExperimentDataforR/FullStudy/PhysMeasures/Richieoutput/wfdbscript.sh
+++ ExperimentDataforR/FullStudy/PhysMeasures/Richieoutput/wfdbscript.sh
@@ -5,5 +5,5 @@ outfiles=`echo $files|sed 's/.txt//g' `
 for i in $files
 do 
     echo $i
-    wrsamp -z -F 10000 -i $i -o $(echo $i |sed 's/.txt//g') -s 1
+     wrsamp -z -F 10000 -i $i  -o $(echo $i |sed 's/.txt//g') 1
 done
