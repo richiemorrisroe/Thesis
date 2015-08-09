87adf01368b7f059a21764a6736e2d0659b2a2fa
Richie Morrisroe <morrisroe@fb.com>
2015-08-09 16:47:23 +0100

    some R package dependencies

---
+++ config.R
@@ -0,0 +1,3 @@
+install.packages(c("ggplot2","psych","GPArotation","xtable","OpenMx","car","arm","caret","nFactors","bcv","glmnet","mokken","eRm","ltm","lavaan","semPlot","reshape2","plyr" "dplyr", "QuantPsyc"))
+setRepositories()
+install.packages("Rgraphviz")
